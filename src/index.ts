411ba4c7fe88ed6770a97602b920179a1ea80085
Yohann THEPAUT <contact@ythepaut.com>
2024-04-26 19:39:59 +0200

    feat(OCLT-23): add CSPRNG wrapper and type conversion functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-throw new Error("There are no entry-point. Please refer to the documentation for usage");
+// @ts-nocheck
+import {
+    ByteArray,
+    Hex,
+    Base64,
+    bytesToHex,
+    hexToBytes,
+    bytesToBase64,
+    base64ToBytes,
+    base64ToHex,
+    hexToBase64
+} from "./types";
+import { randomBytes } from "./random";
