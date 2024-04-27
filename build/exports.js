1961b3fa945bc8d73b5efe06c08a02063b8b7632
Yohann THEPAUT <contact@ythepaut.com>
2024-04-27 21:24:17 +0200

    feat(OCLT-29): add HMAC wrapper

--- build/exports.js
+++ build/exports.js
@@ -3,3 +3,4 @@ export { randomBytes } from "@occult-app/crypto/random";
 export { sha256, sha512 } from "@occult-app/crypto/sha2";
 export { kdf } from "@occult-app/crypto/kdf";
 export { ppf } from "@occult-app/crypto/ppf";
+export { hmac } from "@occult-app/crypto/hmac";
---
+++ tests/hmac.spec.ts
@@ -0,0 +1,17 @@
+import { ByteArray, bytesToHex, hexToBytes } from "../src/types";
+import { hmac } from "../src/hmac";
+
+describe("HMAC", () => {
+    it("should compute the HMAC-SHA256 hash", async () => {
+        const key: ByteArray = new Uint8Array([
+            108, 169, 151, 124, 154, 55, 16, 255, 152, 230, 112, 0, 136, 80, 171, 197, 35, 79, 55,
+            37, 161, 144, 41, 184, 148, 82, 137, 236, 132, 147, 213, 9
+        ]);
+
+        const tag: ByteArray = await hmac(key, hexToBytes("deadbeef"));
+
+        expect(bytesToHex(tag)).toEqual(
+            "e17dec46db65352eb08841a383eaee78b234e3396fa035a5e2359abea0aa4d72"
+        );
+    });
+});
